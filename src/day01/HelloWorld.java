4f0de2cab1dc96f01f86d585774208fcd1bf339e
Hanan Jaluta <hanan.k.jaluta@gmail.com>
2025-03-04 13:49:13 -0500

    HelloCydeo practice

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -1,4 +1,17 @@
 package day01;
 
 public class HelloWorld {
+    /* if the class color red thats mean it does not adde to staging
+    area
+
+    so
+    1-  working directory : create the project and
+    2- we add the class on staging area by (add) the color will change.
+    3- to save it in the local repository by (commit) after we added.
+    4- we push it to remote repository to have copy in gitHub by (push).
+
+local repository is your own git in intellij
+remote repository is GitHub.
+
+    */
 }
