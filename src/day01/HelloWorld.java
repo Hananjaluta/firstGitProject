cd8848db9f3051789f1da279a8f6aa7235530d02
Hanan Jaluta <hanan.k.jaluta@gmail.com>
2025-03-04 13:56:56 -0500

    HelloWorld updated

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -1,17 +1,26 @@
 package day01;
 
 public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("heloo world");
+    }
     /* if the class color red thats mean it does not adde to staging
     area
 
     so
-    1-  working directory : create the project and
-    2- we add the class on staging area by (add) the color will change.
+    1-  working directory : create the project and chnge VCS to git
+    2- we add the class on staging area by (add) the color will change.not red anymore.
     3- to save it in the local repository by (commit) after we added.
+    4-before we push it we have to create a new repository in gitHub same name of the project.
     4- we push it to remote repository to have copy in gitHub by (push).
 
 local repository is your own git in intellij
 remote repository is GitHub.
 
+if the class name red : that's mean we didn't added in staging area
+if the class name white : that's mean it added.
+if the class name blue : that's mean we didn't commit or save the new change.
     */
 }
