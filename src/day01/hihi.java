9d6f6bb2175f78e741b16d15116e4108720c66fd
Hananjaluta <123709562+Hananjaluta@users.noreply.github.com>
2025-03-16 11:25:12 -0400

    Update hihi.java

--- src/day01/hihi.java
+++ src/day01/hihi.java
@@ -3,5 +3,8 @@ package day01;
 public class hihi {
     public static void main(String[] args) {
         System.out.println("hi every body just practice");
+        System.out.println("yah");
+
+        
     }
 }
