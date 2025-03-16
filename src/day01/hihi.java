44428df9d8c125a5d5fb9b127c33a9cb7a796f74
Hanan Jaluta <hanan.k.jaluta@gmail.com>
2025-03-16 11:22:46 -0400

    HelloWorld updated

--- src/day01/hihi.java
+++ src/day01/hihi.java
@@ -3,5 +3,6 @@ package day01;
 public class hihi {
     public static void main(String[] args) {
         System.out.println("hi every body just practice");
+        System.out.println("some lines ");
     }
 }
